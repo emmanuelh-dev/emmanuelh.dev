c9304f3f9a37f1959eb7048da67e64674b40417d
Emmanuel Hernandez <e805177@gmail.com>
2023-08-01 13:23:11 -0600

    feat: tailwind gallery starter

--- data/projectsData.js
+++ data/projectsData.js
@@ -12,6 +12,12 @@ const projectsData = [
     imgSrc: '/static/images/creative.svg',
     href: 'https://bysmax.com',
   },
+  {
+    title: 'LA CAFETERIA | STARTER',
+    description: `Discover our restaurant template crafted with Tailwind CSS and Gatsby. Browsing our menu is now a breeze, making the dining experience hassle-free and enjoyable.`,
+    imgSrc: '/static/images/gallery.png',
+    href: 'https://la-cafeteria-with-tailwindcss.vercel.app/',
+  },
 ]
 
 export default projectsData
