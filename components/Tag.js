a6b29076204d6a37a6cf9d59b80b090462df1a83
Emmanuel Hernandez <e805177@gmail.com>
2023-08-12 03:58:28 -0600

    update: update version of next and tags

--- components/Tag.js
+++ components/Tag.js
@@ -3,10 +3,11 @@ import kebabCase from '@/lib/utils/kebabCase'
 
 const Tag = ({ text }) => {
   return (
-    <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-        {text.split(' ').join('-')}
-      </a>
+    <Link
+      href={`/tags/${kebabCase(text)}`}
+      className="mr-3 rounded-full bg-primary-700 px-2.5 py-0.5 text-sm text-white"
+    >
+      {text.split(' ').join('-')}
     </Link>
   )
 }
