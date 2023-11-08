98eda6b03195b0c3f235162bddc7d76677307c7f
Emmanuel Hernandez <e805177@gmail.com>
2023-11-08 15:59:40 -0600

    update README.md and Experience.js

--- README.md
+++ README.md
@@ -4,11 +4,6 @@
 
 # Tailwind Nextjs Starter Blog
 
-[![GitHub Repo stars](https://img.shields.io/github/stars/emmanuelh-dev/emmanuelh.dev)](https://GitHub.com/timlrx/tailwind-nextjs-starter-blog/stargazers/)
-[![GitHub forks](https://img.shields.io/github/forks/emmanuelh-dev/emmanuelh.dev)](https://GitHub.com/timlrx/tailwind-nextjs-starter-blog/network/)
-[![Twitter URL](https://img.shields.io/twitter/url?style=social&url=https%3A%2F%2Ftwitter.com%2Ftimlrxx)](https://twitter.com/timlrxx)
-[![Sponsor](https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=GitHub&link=https://github.com/sponsors/timlrx)](https://github.com/sponsors/timlrx)
-
 [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/timlrx/tailwind-nextjs-starter-blog)
 
 This is a [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/) blogging starter template. Probably the most feature-rich Next.js markdown blogging template out there. Comes out of the box configured with the latest technologies to make technical writing a breeze. Easily configurable and customizable. Perfect as a replacement to existing Jekyll and Hugo individual blogs.
--- components/Experience.js
+++ components/Experience.js
@@ -1,34 +1,53 @@
 import React from 'react'
 import { DiNodejs, DiPython, DiPhp, DiLaravel, DiHtml5, DiCss3, DiJsBadge } from 'react-icons/di'
 import { SiNextdotjs } from 'react-icons/si'
+
 const experience = [
   {
-    title: 'Gonzher Solutions',
+    title: 'Gonzher Logistic and transport',
     job: 'Full Stack Developer',
     date: 'Oct 2023 - Present',
-    skills: ['NodeJS', 'JavaScript', 'Git', 'Php', 'Laravel'],
-  },
-  {
-    title: 'Little Minds',
-    job: 'Robotic Professor',
-    date: 'Oct 2023 - Present',
-    skills: [],
+    skills: ['NodeJS', 'JavaScript', 'Git', 'Php', 'Laravel', 'UX'],
   },
   {
     title: 'ConnectIT',
     job: 'Developer',
     date: 'May 2023 - Sep 2023',
-    skills: ['Nodejs', 'JS', 'Vue', 'Vuetify', 'Git', 'UX', 'FastAPI', 'Python'],
+    skills: ['Nodejs', 'JavaScript', 'Vue', 'Vuetify', 'Git', 'UX', 'FastAPI', 'Python'],
   },
   {
     title: 'BysMax',
     job: 'FrontEnd Developer',
     date: 'Jan 2021 - June 2023',
-    skills: ['React', 'JS', 'NextJS', 'Node', 'TailwindCSS'],
+    skills: ['React', 'JavaScript', 'NextJS', 'Node', 'TailwindCSS'],
   },
 ]
 
+function calcularPorcentajeUso() {
+  const habilidadesTotales = experience.reduce((total, trabajo) => total.concat(trabajo.skills), [])
+
+  return experience.map((trabajo) => {
+    const porcentajesHabilidades = trabajo.skills.reduce((porcentajes, habilidad) => {
+      const usoPorcentaje = (
+        (habilidadesTotales.filter((h) => h === habilidad).length / habilidadesTotales.length) *
+        100
+      ).toFixed(2)
+      porcentajes[habilidad] = usoPorcentaje
+      return porcentajes
+    }, {})
+
+    return {
+      title: trabajo.title,
+      job: trabajo.job,
+      date: trabajo.date,
+      skills: porcentajesHabilidades,
+    }
+  })
+}
+
 const Experience = () => {
+  const experienciaConPorcentajes = calcularPorcentajeUso()
+
   return (
     <div>
       <div className="space-y-2 pb-8 pt-6 md:space-y-5">
@@ -37,20 +56,32 @@ const Experience = () => {
         </h1>
       </div>
       <section className="ml-4 lg:flex">
-        <ol class="relative border-l">
-          {experience.map((company) => (
-            <li class="mb-10 ml-4" key={company.title}>
-              <div class="border-tert bg-tert absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border bg-white"></div>
-              <time class="mb-1 font-normal leading-none opacity-50">{company.date}</time>
-              <h3 class="text-lg font-bold">{`${company.job} - ${company.title}`}</h3>
-              <ul class="mb-4 font-normal opacity-75">
-                {company.skills.map((skill) => (
-                  <li key={skill} className=" ml-4 list-disc text-sm">
-                    {skill}
-                  </li>
-                ))}
-              </ul>
-            </li>
+        <ol class="relative flex-1 border-l">
+          {experienciaConPorcentajes.map((company) => (
+            <div className="flex" key={company.title}>
+              <li class="mb-10 ml-4 flex-1">
+                <div class="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border bg-white"></div>
+                <time class="mb-1 font-normal leading-none opacity-50">{company.date}</time>
+                <h3 class="text-lg font-bold">{`${company.job} - ${company.title}`}</h3>
+                <ul class="mb-4 font-normal opacity-75">
+                  {Object.entries(company.skills).map(([skill, porcentaje, index]) => (
+                    <div className="flex justify-between" key={skill}>
+                      <li className=" ml-4 list-disc text-sm">{skill}</li>
+                      <span
+                        key={index}
+                        className={`mr-4 list-disc text-sm ${
+                          porcentaje > 15
+                            ? 'w-32 bg-green-600'
+                            : porcentaje > 10
+                            ? 'w-24 bg-green-400'
+                            : 'w-14 bg-green-200'
+                        } block h-[0.4rem]  rounded-full`}
+                      ></span>
+                    </div>
+                  ))}
+                </ul>
+              </li>
+            </div>
           ))}
         </ol>
       </section>
